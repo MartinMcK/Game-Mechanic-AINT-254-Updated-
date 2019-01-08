75449ff048e0fdae8c4f4b742dc938725aa27c56
MartinMcK <33032026+MartinMcK@users.noreply.github.com>
2019-01-08 13:52:59 +0000

    Extras added, e.g. sound

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    public AudioSource deathSound;
+
+	// Use this for initialization
+	void Start () {
+
+        deathSound = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            deathSound.Play();
+
+            
+        }
+    }
+}
